07cfedd8b2b6048a6183248349404ad85edee3cd
Jérémie Horhant <jeremie.horhant@titinux.net>
2012-02-09 13:18:41 +0100

    Show flash messages.

--- app/helpers/groseillier/groseillier_helper.rb
+++ app/helpers/groseillier/groseillier_helper.rb
@@ -5,5 +5,20 @@ module Groseillier
         content_tag :h1, title
       end
     end
+
+    def render_flash_messages
+      capture_haml do
+        flash.each do |name, msg|
+          haml_tag "p.flash.#{name}" do
+            haml_concat image_tag("groseillier/icons/interactions/#{name}.png")
+            haml_tag :strong do
+              haml_concat t("flash.names.#{name}")
+            end
+
+            haml_concat " : #{msg}"
+          end
+        end
+      end
+    end
   end
 end
--- lib/groseillier/engine.rb
+++ lib/groseillier/engine.rb
@@ -9,5 +9,7 @@ module Groseillier
       g.stylesheet_engine   :sass
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
+
+    config.i18n.load_path += Dir[config.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
